a344a05a45f4a3ec75d6ceff5593c98c6d141b03
Damián Pumar <damianpumar@gmail.com>
2017-08-24 09:51:34 -0300

    First commit

---
+++ TDD.Kata.StringCalculator/TDD.Kata.StringCalculator.Tests.Unit/UnitTestExample.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace TDD.Kata.StringCalculator.Tests.Unit
+{
+    public class UnitTestExample
+    {
+        //private Target Target;
+
+        public UnitTestExample()
+        {
+            //this.Target = new StringCalculator();
+        }
+
+        [Fact]
+        public void Example()
+        {
+            //Arrange
+            //Your set up!.
+
+            //Act
+            //Your principal action to test.
+
+            //Assert
+            //Your expected value.
+        }
+    }
+}
